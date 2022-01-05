fc412de251392a44fc36da05df60b3d59ab42bbf
Davi Lacerda <dlacerda9@gmail.com>
2022-01-05 17:51:39 -0300

    try to fix globalstyles error

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import { GlobalStyles } from "./GlobalStyles";
 import RoutesSite from "./Routes";
 
 function App() {
   return (
     <>
-      <GlobalStyles></GlobalStyles>
       <RoutesSite></RoutesSite>
     </>
   );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,9 +3,12 @@ import Header from "../../components/Header/Header";
 import MainSlider from "../../components/MainSlider/MainSlider";
 import CompanyCard from "../../components/CompanyCard/CompanyCard";
 
+import { GlobalStyles } from "../../GlobalStyles";
+
 function Home() {
     return (
         <>
+            <GlobalStyles></GlobalStyles>
             <Header></Header>
             <MainSlider></MainSlider>
             <CompanyCard></CompanyCard>
